3940e64d1ef16ac9f475fa9af569a8957447bd64
Elda Moudy <elda@moudy.net>
2025-08-06 17:21:28 -0400

    Updating the resource file

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,17 @@
 import { defineAuth } from '@aws-amplify/backend';
+import { postConfirmation } from './post-confirmation/resource';
 
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ * @see https://aws.amazon.com/getting-started/hands-on/build-web-app-s3-lambda-api-gateway-dynamodb/module-four/
  */
+
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  triggers: {
+    postConfirmation
+  }
 });
